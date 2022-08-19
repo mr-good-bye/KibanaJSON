4c6ee1dabd34ee9b941459687b1b1f9fdd052e63
Nikita Trubitcyn <ntrubitsyn@pmpractice.ru>
2022-08-19 04:52:37 +0300

    Improvement of file-name generation

--- KibanaUniqueJson.py
+++ KibanaUniqueJson.py
@@ -1,9 +1,10 @@
 import json
 import os
 
+
 jsonFile = "KibanaErrorsHelper.json"
-t1 = ' :<>/\\|?*"'
-t2 = '_--------_'
+t1 = ':<>/\\|?*"'
+t2 = '--------_'
 
 
 def file_enter():
@@ -13,7 +14,6 @@ def file_enter():
 
 def handler(data):
     json_data = data[:data.rfind('}')+1]
-    print(len(json_data))
     message = data[data.rfind('}')+1:]
     json_data = json.loads(json_data)
     json_as_string = json.dumps(json_data, indent=4, ensure_ascii=False).encode('utf-8').decode()
@@ -22,6 +22,30 @@ def handler(data):
     json_as_string = json_as_string.replace("\\t", "    ")
     json_as_string += message
     return json_as_string
+
+
+def create_name(host: str, timestamp: str):
+    #  timestamp: YYYY-MM-DD'T'HH-mm-ss.SSS'Z'(-3hours)
+    tt = timestamp.maketrans(t1, t2)
+    timestamp = timestamp.translate(tt)
+    _timestamp = timestamp.split('-')  # ['2022', '08', '15T10', '50', '12.413Z']
+    timestamp = {
+        'year': int(_timestamp[0]),
+        'month': int(_timestamp[1]),
+        'day': int(_timestamp[2].split('T')[0]),
+        'hour': int(_timestamp[2].split('T')[1]) + 3,
+        'minute': int(_timestamp[3]),
+        'seconds': _timestamp[4][:6]
+    }
+    if timestamp['hour'] > 23:
+
+        timestamp['hour'] -= 24
+        timestamp['day'] += 1
+        # TODO: Maybe if day > 30/31/28/29 => month++?
+    res = f"{timestamp['year']}.{timestamp['month']}.{timestamp['day']}"
+    res += f"__{timestamp['hour']}-{timestamp['minute']}-{timestamp['seconds']}"
+    res += f"__{host}"
+    return res
 
 
 def main():
@@ -35,10 +59,9 @@ def main():
         host = json_data["_source"]["host"]["name"]
         json_as_string = handler(json_data["_source"]["message"])
         message = json_as_string[json_as_string.rfind('}')+1:]
-        file_name = f"error_{host}_{timestamp}_{message[message.rfind(':')+1:]}.json"
-        print(message)
+        file_name = f"{create_name(host, timestamp)}{message[message.rfind(':')+1:]}.json"
         tt = file_name.maketrans(t1, t2)
-        with open('errorJson/_' + file_name.translate(tt), 'w', encoding='utf-8') as f:
+        with open('errorJson/' + file_name.translate(tt), 'w', encoding='utf-8') as f:
             f.write(json_as_string)
     except Exception as e:
         print(e)
